0f6e996cf37b18a3f0f6949e163c2bd4eaafac5f
CringeKid101 <sooraj2awesome@gmail.com>
2025-08-06 19:15:25 +0530

    Inital Commit

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private AppDbContext _context { get; set; }
+        public MembersController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await _context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await _context.Users.FindAsync(id);
+            if (member == null)
+            {
+               return NotFound();
+            }
+            return member;
+
+        }
+    }
+}
